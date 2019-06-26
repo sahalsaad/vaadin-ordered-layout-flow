405cfaeaa749ad69f7dc50fa9a7456a7ce2cf92c
sahalsaad <caalshift@gmail.com>
2019-06-26 22:12:18 +0800

    Add unit test

--- vaadin-ordered-layout-flow/src/test/java/com/vaadin/flow/component/orderedlayout/tests/LayoutDefaultsTest.java
+++ vaadin-ordered-layout-flow/src/test/java/com/vaadin/flow/component/orderedlayout/tests/LayoutDefaultsTest.java
@@ -2,6 +2,8 @@ package com.vaadin.flow.component.orderedlayout.tests;
 
 import java.util.stream.Stream;
 
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -78,19 +80,94 @@ public class LayoutDefaultsTest {
     }
 
     @Test
-    public void testFlexLayout_setAndRemoveAlignContent() {
+    public void testFlexLayout_setAndUnsetAlignContent() {
         FlexLayout layout = new FlexLayout();
         FlexLayout.ContentAlignment contentAlignment =
                 FlexLayout.ContentAlignment.CENTER;
         layout.setAlignContent(contentAlignment);
 
-        Assert.assertTrue("should set align-content",
-                layout.getElement().getStyle().get("align-content")
-                        .equalsIgnoreCase(contentAlignment.toString()));
+        Assert.assertEquals("should set align-content",
+                layout.getAlignContent(), contentAlignment);
 
         layout.setAlignContent(null);
-        Assert.assertNull("should remove align-content",
-                layout.getElement().getStyle().get("align-content"));
+        Assert.assertEquals("should return stretch if no align-content set",
+                layout.getAlignContent(), FlexLayout.ContentAlignment.STRETCH);
+    }
+
+    @Test
+    public void testFlexLayout_setAndRemoveFlexBasis() {
+        FlexLayout layout = new FlexLayout();
+        Div div = new Div();
+        layout.add(div);
+        layout.setFlexBasis("10px", div);
+
+        Assert.assertEquals("should set flex-basis",
+                layout.getFlexBasis(div), "10px");
+
+        layout.setFlexBasis(null, div);
+        Assert.assertNull("should remove flex-basis from component",
+                layout.getFlexBasis(div));
+    }
+
+    @Test
+    public void testFlexLayout_setAndUnsetFlexDirection() {
+        FlexLayout layout = new FlexLayout();
+        FlexComponent.FlexDirection direction = FlexComponent.FlexDirection.ROW_REVERSE;
+        layout.setFlexDirection(direction);
+
+        Assert.assertEquals("should set flex-direction",
+                layout.getFlexDirection(layout), direction);
+
+        layout.setFlexDirection(null);
+        Assert.assertEquals("should return row if no flex-direction set",
+                layout.getFlexDirection(layout), FlexLayout.FlexDirection.ROW);
+    }
+
+    @Test
+    public void testFlexLayout_setFlexShrink() {
+        FlexLayout layout = new FlexLayout();
+        Div div = new Div();
+        layout.add(div);
+        layout.setFlexShrink(2, div);
+
+        Assert.assertEquals("should set flex-shrink",
+                layout.getFlexShrink(div), 2, 0);
+    }
+
+    @Test
+    public void testFlexLayout_getFlexShrink_returnOneIfNotSet() {
+        FlexLayout layout = new FlexLayout();
+        Div div = new Div();
+        layout.add(div);
+
+        Assert.assertEquals("should return 1 if flex-shirk not set",
+                layout.getFlexShrink(div), 1, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFlexLayout_setFlexShrink_throwExceptionIfNegative() {
+        FlexLayout layout = new FlexLayout();
+        Div div = new Div();
+        layout.add(div);
+        layout.setFlexShrink(-1, div);
+    }
+
+    @Test
+    public void testFlexLayout_setAndUnsetOrder() {
+        FlexLayout layout = new FlexLayout();
+        Div div = new Div();
+        layout.add(div);
+
+        Assert.assertEquals("should return 0 if no order set",
+                layout.getOrder(div), 0);
+
+        layout.setOrder(1, div);
+        Assert.assertEquals("should set order",
+                layout.getOrder(div), 1);
+
+        layout.setOrder(0, div);
+        Assert.assertEquals("should unset order",
+                layout.getOrder(div), 0);
     }
 
     private void testExpandableComponent(String size,
